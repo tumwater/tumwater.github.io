26c4d4c3992e9c3c89a81dd67f2f4834de9e3b9d
tumwater <83632218+tumwater@users.noreply.github.com>
2023-03-16 20:42:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -42,21 +42,21 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelOneTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
     clue2:{
       x:2,
-      y:1
+      y:1,
   },
     clue3:{
       x:3,
-      y:1
+      y:1,
    }
    };
 
@@ -85,22 +85,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelTwoTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    
    };
   // third level
@@ -128,22 +128,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelThreeTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    };
 
    // fourth level
@@ -171,22 +171,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelFourTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    };
 
    // fifth level
@@ -214,22 +214,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelFiveTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    };   
 
    // sixth level
@@ -257,22 +257,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelSixTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    };   
 
    // seventh level
@@ -300,22 +300,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelSevenTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    };   
 
    // eighth level
@@ -343,22 +343,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelEightTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    };
 
    // ninth level
@@ -386,22 +386,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelNineTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    };
 
    // tenth level
@@ -429,22 +429,22 @@ for (i = 0; i < acc.length; i++) {
         y:0
     },
     goal:{
-        x:1,
+        x:0,
         y:0
     },    
     theme:'levelTenTheme',
     clue1:{
       x:1,
-      y:1
+      y:1,
    },
-   clue2:{
-     x:2,
-     y:1
- },
-   clue3:{
-     x:3,
-     y:1
-  }
+    clue2:{
+      x:2,
+      y:1,
+  },
+    clue3:{
+      x:3,
+      y:1,
+   }
    };
 /*
  *  The game object constructor.
@@ -457,7 +457,7 @@ function Game(id,level) {
   
   // level addition
   this.level_idx = 0;
-  
+
   // establish the basic properties common to all this objects.
   this.tileTypes = ['floor','wall'];
   this.tileDim = 32;
@@ -716,6 +716,32 @@ Game.prototype.checkGoal = function() {
      }
 }
 
+function showClueOne() {
+   const tooltip = document.querySelector(".ClueOne");
+   tooltip.style.display = "block";
+}
+function hideClueOne() {
+   const tooltip = document.querySelector(".ClueOne");
+   tooltip.style.display = "none";
+}  
+function showClueTwo() {
+  const tooltip = document.querySelector(".ClueTwo");
+  tooltip.style.display = "block";
+}
+function hideClueTwo() {
+  const tooltip = document.querySelector(".ClueTwo");
+  tooltip.style.display = "none";
+}  
+
+function showClueThree() {
+  const tooltip = document.querySelector(".ClueThree");
+  tooltip.style.display = "block";
+}
+function hideClueThree() {
+  const tooltip = document.querySelector(".ClueThree");
+  tooltip.style.display = "none";
+}  
+
 Game.prototype.checkClue1 = function() {
   let body = document.querySelector('body');
 
@@ -723,9 +749,11 @@ Game.prototype.checkClue1 = function() {
     this.player.x == this.clue1.x) {
     
       console.log("success1")
+      showClueOne();
+
   }
   else {
-
+    hideClueOne();
   }
 }
 
@@ -736,9 +764,10 @@ Game.prototype.checkClue2 = function() {
     this.player.x == this.clue2.x) {
     
       console.log("success2")
+      showClueTwo();
   }
   else {
-
+    hideClueTwo();
   }
 }
 
@@ -749,9 +778,10 @@ Game.prototype.checkClue3 = function() {
     this.player.x == this.clue3.x) {
     
       console.log("success3")
+      showClueThree();
   }
   else {
-
+    hideClueThree();
   }
 }
 /*
@@ -765,8 +795,6 @@ Game.prototype.changeLevel = function() {
     // if higher than max index, set back to zero.
        if (this.level_idx > levels.length -1) {
          this.level_idx = 0;
-        alert("you won")
-        location.reload()
     }
     
     // get the level at this index.
@@ -786,7 +814,61 @@ Game.prototype.changeLevel = function() {
     this.clue1 = {...level.clue1};
     this.clue2 = {...level.clue2};
     this.clue3 = {...level.clue3};
+
+    switch (this.level_idx) {
+      case 0:
+        document.getElementById("ClueOne").innerHTML = ("Freedom of Speech & Press: The First Amendment has multiple parts. The first part of the first Amendment is the freedom of speech and press. These rights are together known as free expression. Both of these rights bring responsibility. For freedom of the press you cannot spread false accusations or publish information that may help enemies in war. For freedom of speech, the first amendment allows free speech in public places like streets and parks. However, that power is not unlimited. Speech that endangers the public is strictly prohibited.");
+        document.getElementById("ClueTwo").innerHTML = ("Freedom to use Public Property: Citizens can use public property for meetings and demonstrations. The first amendment does not give people the right to close streets or buildings or protest violently.");
+        document.getElementById("ClueThree").innerHTML = ("Freedom to Petition the Government & Religion : The declaration of independence noted King George III repeatedly ignored petitions. This time congress didn't want that to happen again, so they made sure people had the right to petition the government, and made sure they responded to them. The First Amendment also states that Congress cannot make a law respecting or establishing a religion as “official”.");
+        break;
+      case 1:
+        document.getElementById("ClueOne").innerHTML = ("Right to have weapons: America preferred to have local volunteer state militias to protect the nation rather than have a  standing army, granting people the right to bear arms");
+        document.getElementById("ClueTwo").innerHTML = ("Right to own a gun for personal use & defense: An individual has the right to own a gun for personal use and to defend themselves in their home.");
+        document.getElementById("ClueThree").innerHTML = ("Fun Fact: The 2nd amendment was actually a compromise with the goal of protecting the freedom of states to retain their own militias as well as the right of citizens to keep and bear arms for personal defense and national defense. ");
+        break;
+      case 2:
+        document.getElementById("ClueOne").innerHTML = ("No Quartering: This amendment outlines how soldiers aren’t allowed to be quartered in someone's property without their consent");
+        document.getElementById("ClueTwo").innerHTML = ("Fun Fact: Before the American Revolution, King George issued soldiers to be quartered in people’s homes. The Americans despised this and this is why this amendment was made");
+        document.getElementById("ClueThree").innerHTML = ("Fun Fact #2: Patrick Henry said that quartering was “one of the principle reasons for dissolving the connection with Great Britain”");
+        break;       
+       case 3:
+        document.getElementById("ClueOne").innerHTML = ("Unreasonable Searches: The 4th amendment protects people from unreasonable searches and seizures. The police must need evidence to search you or your property! ");
+        document.getElementById("ClueTwo").innerHTML = ("Good Reason for Arrest: Before arrests, police need to show a judge a good reason for allowing an action.  The judge will issue a warrant specifically saying who will be arrested or what will be searched");
+        document.getElementById("ClueThree").innerHTML = ("Example of 4th Amendment: The police require a warrant before they can search your house");
+        break;  
+      case 4:
+        document.getElementById("ClueOne").innerHTML = ("Right to Grand Jury: People accused of major crimes have the right to a grand jury hearing.");
+        document.getElementById("ClueTwo").innerHTML = ("No Repeat Trials: A person who is accused of a crime and is found not guilty, cannot be tried again for that same crime.");
+        document.getElementById("ClueThree").innerHTML = ("Remain Silent: The police cannot force a person to self-incriminate themselves. This means that they cannot force people to say things that could be used against them in court");
+        break;  
+       case 5:
+        document.getElementById("ClueOne").innerHTML = ("The 6th amendment provides accused people fair trials. One part of the 6th amendment, is the right to a speedy trial, meaning that the accused criminal cannot be kept in jail for long periods of time waiting for trial. This also gives witnesses a fresher memory of what happened ");
+        document.getElementById("ClueTwo").innerHTML = ("The 6th amendment also gives the right to the accused to be judged by a jury of his or people living in his area. The jury must be impartial, meaning they must have no bias towards the defendant. During the trial, the 6th amendment also makes sure that the judge must tell the defendant the charge and the time and place of the crime.");
+        document.getElementById("ClueThree").innerHTML = ("A defendant has the right to hear and question all witnesses who testify at the trial. They also have the right to an attorney to assist them in the case. ");
+        break;  
+        case 6:
+          document.getElementById("ClueOne").innerHTML = ("Not all trials involve criminal actions. Some civil trials settle disputes between businesses or people. ");
+          document.getElementById("ClueTwo").innerHTML = ("Civil cases typically involve money, property, or family matters.");
+          document.getElementById("ClueThree").innerHTML = ("The seventh amendment states that “no fact tried by a jury shall be otherwise reexamined” this means that no judge can override a jury’s decision.");
+          break;
+        case 7:
+          document.getElementById("ClueOne").innerHTML = ("The 8th amendment protects the accused person’s right before and after a trial.");
+          document.getElementById("ClueTwo").innerHTML = ("Before a trial, a judge is forbidden from demanding excessive bail.");
+          document.getElementById("ClueThree").innerHTML = ("After a trial, a judge is forbidden from excessive fines and cruel and unusual punishment.");
+          break;
+        case 8:
+          document.getElementById("ClueOne").innerHTML = ("The ninth amendment states that even though some rights are included in the constitution, other rights that are not listed are retained by the people");
+          document.getElementById("ClueTwo").innerHTML = ("An example of this is the right to privacy. Although it is not included in the constitution, it is a right that is retained by the people");
+          document.getElementById("ClueThree").innerHTML = ("Another example is the 1973 case of Roe versus Wade. They stated that women had privacy rights over their body, which then made abortion legal in America.");
+          break;       
+         case 9:
+          document.getElementById("ClueOne").innerHTML = ("The tenth amendment gives power to the states and the people so that the government doesn’t have too much power.");
+          document.getElementById("ClueTwo").innerHTML = ("Reserved power allows the states to make different laws in their own states such as speed limits, and their own elections.");
+          document.getElementById("ClueThree").innerHTML = ("The tenth amendment gives your state the power to issue business licenses and make sure that your doctor has practiced medicine in your state.");
+          break;
+    }
  }
+
 
  /*
   * If goal has been reached, 
@@ -810,8 +892,6 @@ Game.prototype.changeLevel = function() {
        obj.player.x != obj.goal.x) {
          return;
        }
-
-
 
        // change level of game object by changing it's properties
        obj.changeLevel();
